63a5a29b78922d4a78512534c89529d118154b73
Pamavoc <pamavoc@gmail.com>
2024-11-17 15:17:21 +0100

    first commit

---
+++ app/components/ui/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Hello Gobelins</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+div {
+  background: $mainColor;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  p {
+    color: white;
+    font-size: 24px;
+  } 
+}
+</style>
