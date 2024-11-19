d546c8d149c0c89572ae0cd10484cc67a1fa6585
Pamavoc <pamavoc@gmail.com>
2024-11-19 23:03:35 +0100

    counter ref

---
+++ app/components/ui/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>Compteur : {{ count }}</h1>
+      <button @click="increment">Incrémenter</button>
+      <button @click="reset">Réinitialiser</button>
+    </div>
+  </template>
+  
+  <script setup>
+  // rend la valeur réactive
+  import { ref } from "vue";
+  
+  // on crée une constante car elle est "immutable", c'est la valeur dedans qui va changer
+  const count = ref(0);
+  // la valeur est accessible par .value car on accède à la valeur réactive du ref
+
+  const increment = () => {
+    count.value++;
+  };
+  
+  const reset = () => {
+    count.value = 0;
+  };
+  </script>
+  
