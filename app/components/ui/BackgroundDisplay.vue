28dcc17a6dbad003141518894b51ac62fd07e745
Pamavoc <pamavoc@gmail.com>
2024-11-19 22:51:45 +0100

    parent child communication

---
+++ app/components/ui/BackgroundDisplay.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="background-display">
+        <ColorSelector onChange />
+    </div>
+</template>
+
+<script setup lang="ts">
+import ColorSelector from './ColorSelector.vue';
+</script>
+
+<style scoped lang="scss">
+</style>
